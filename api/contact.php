4190da6d8818815d76ecad4a5bea7573942382d9
MDK-13 <mariodilipkumar@gmail.com>
2025-11-25 11:00:17 +0800

    Updates

---
+++ api/contact.php
@@ -0,0 +1,17 @@
+<?php
+$data = json_decode(file_get_contents("php://input"), true);
+
+$name = $data["name"];
+$email = $data["email"];
+$message = $data["message"];
+
+$response = [
+  "status" => "success",
+  "received" => [
+    "name" => $name,
+    "email" => $email,
+    "message" => $message
+  ]
+];
+
+echo json_encode($response);
---
+++ api/data.php
@@ -0,0 +1,6 @@
+<?php
+echo json_encode([
+  "date" => date("Y-m-d H:i:s"),
+  "location" => "Kuala Lumpur",
+  "temp" => "28°C"
+]);
